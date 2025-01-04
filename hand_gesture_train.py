7fb5ae92029b100cc2b354dc1b6c2959882f409b
arwinder <arwinderjaspal@gmail.com>
2025-01-05 01:21:25 +0800

    Created a sequential CNN model with 5 convolutional layers, each followed by max pooling, to extract features from 256x256 RGB images

---
+++ hand_gesture_train.py
@@ -0,0 +1,19 @@
+from keras.api.models import Sequential
+from keras.api.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras._tf_keras.keras.preprocessing.image import ImageDataGenerator
+
+# Create a Sequential model
+model = Sequential()
+
+# Add convolutional layers to the model
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(256, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+
+
